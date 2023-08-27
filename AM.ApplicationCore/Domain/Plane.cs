3c86a9b851f91c87dc2726e9c39452e7323aa329
Zied.mathlouthi@esprit.tn <zied.mathlouthi@esprit.tn>
2023-08-28 00:05:51 +0100

    View Edit

--- AM.ApplicationCore/Domain/Plane.cs
+++ AM.ApplicationCore/Domain/Plane.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,33 +15,31 @@ namespace AM.ApplicationCore.Domain
     }
     public class Plane
     {
-
-
-
-        //public Plane(PlaneType pt, int capacity, DateTime date)
-        //{
-        //    PlaneType = pt;
-        //    Capacity = capacity;
-        //    ManufactureDate = date;
-
-        //}
         public int PlaneId { get; set; }
-
         public PlaneType PlaneType { get; set; }
-
         public DateTime ManufactureDate { get; set; }
         [Range(0, int.MaxValue)]
         public int Capacity { get; set; }
-
+        //prop de navigation
         public virtual List<Flight> Flights { get; set; }
-
-        public virtual List<Seat> Seats { get; set; }
-
-
+        [NotMapped]
+        public string Information { get { return ManufactureDate + " " + Capacity + " " + PlaneId; } }
+        //TP1-Q6: Réimplémenter la méthode ToString()
         public override string ToString()
         {
             return "PlaneType: " + PlaneType + " ManufactureDate: " + ManufactureDate + " Capacity: " + Capacity;
         }
+        //TP1-Q7: Constructure non paramétré
+        public Plane()
+        {
+
+        }
+        //TP1-Q8: Constructure paramétré
+        public Plane(PlaneType pt, int capacity, DateTime date)
+        {
+            PlaneType = pt;
+            Capacity = capacity;
+            ManufactureDate = date;
+        }
     }
 }
-
